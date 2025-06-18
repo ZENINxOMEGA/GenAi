4c15f2f816324c6eb7ba7da1cf342f8cdb4c7022
Aniket Sharma <aniketsharma2914@gmail.com>
2025-06-18 13:56:28 +0530

    Training Data X

---
+++ GenAi/LinearRegression.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+X = pd.read_csv('.C:\OMEGA\Python\GenAi\Linear_X_Train.csv').values.reshape((-1,))
+y = pd.read_csv('."C:\OMEGA\Python\GenAi\Linear_Y_Train.csv"').values.reshape((-1,))
+
+plt.scatter(X,y)
+plt.show()
+
+w1 = 50
+w0 = 1
+lr = 0.1
+yp = w1*X+w0
+plt.scatter(X,y)
+plt.plot(X,yp,c='r')
+plt.show()
